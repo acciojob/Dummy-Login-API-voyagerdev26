2187530451c75fc59d0508b2d516cb515f169e01
voyagerdev26 <devs251898@gmail.com>
2026-04-28 17:18:14 +0530

    version one

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,40 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React,{useState} from "react";
+import LoginForm from "./LoginForm";
+ 
+
+const App = () => {
+
+  let [emailCheck,setEmailCheck]=useState(false);
+  let [passCheck,setPassCheck]=useState(false);
+
+  let data= [
+    {
+        id: 1,
+        name: "ABC",
+        email: "abc@gmail.com",
+        password: "12"
+    },
+    {
+        id: 2,
+        name: "DEF",
+        email: "def@gmail.com",
+        password: "1234"
+    },
+    {
+        id: 3,
+        name: "GHI",
+        email: "ghi@gmail.com",
+        password: "123456"
+    }
+ ]
+  return (
+    <div>
+        <LoginForm data={data} setPassCheck={setPassCheck} setEmailCheck={setEmailCheck}/>
+        {emailCheck && <p id="user-error">User not found</p>}
+        {passCheck && <p id="password-error">Password Incorrect</p>}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LoginForm.js
@@ -0,0 +1,45 @@
+import React,{useState,useRef} from "react";
+
+const LoginForm=({data,setPassCheck,setEmailCheck})=>{
+  let emailRef= useRef("");
+  let passRef= useRef("");
+
+  let [passErr,setPassErr]= useState(false);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    let flag=false;
+    data.forEach(obj=>{
+      if(obj.email===emailRef.current.value.trim() && obj.password===passRef.current.value.trim()){
+        setTimeout(()=>{console.log(obj)},3000);
+        flag=true;
+      }
+      else if(obj.email===emailRef.current.value.trim() && obj.password!==passRef.current.value.trim()){
+        setTimeout(()=>{console.log("Password incorrect")},3000);
+        setPassCheck(true);
+        setPassErr(true);
+        flag=true;
+      }
+    })
+
+    if(!flag){
+      setEmailCheck(true);
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label >Email:</label>
+        <input type="email" ref={emailRef} id="input-email"></input>
+        <br/>
+        <label >Password:</label>
+        <input type="password" ref={passRef} id="input-password"></input>
+        <br/>
+        {passErr && <p>Password incorrect</p>}
+        <button id="submit-form-btn">Submit</button>
+      </form>
+    </div>
+  )
+}
+export default LoginForm;
